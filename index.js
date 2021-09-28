a4102baf35d6b6ca854555d536218ed5a3a3560a
DeathNet123 <arslan.computer2002@gmail.com>
2021-09-28 19:13:26 +0500

    done

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 $(document).ready(() =>{
 
     (() => {
-        $("#c").animate({width: '80%'}).text('80%');
-        $("#py").animate({width: '50%'}).text('50%');
-        $("#js").animate({width: '75%'}).text('75%');
-        $("#jq").animate({width: '55%'}).text('55%');
-        $("#react").animate({width: '20%'}).text('20%');
-        $("#html").animate({width: '85%'}).text('85%');
-        $("#css").animate({width: '70%'}).text('70%');
-        $("#boots").animate({width: '65%'}).text('65%'); 
-        $("#linux").animate({width: '30%'}).text('30%');
-        $("#oop").animate({width: '50%'}).text('50%');
-    })();
-    (() => {
         $("#welcome").modal();
 
     }) ();
